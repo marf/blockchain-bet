ec1d32110abe23ac83d05ce03c9b468d9a6d31ef
Marco Fontana <fontana.marco@hotmail.com>
2019-01-11 13:17:44 +0100

    Fix smart contract

--- contracts/BetContract.sol
+++ contracts/BetContract.sol
@@ -28,20 +28,18 @@ contract BetContract {
       return false;
     }
 
-    function placeBet(uint8 _team) public payable {
-      //The first require is used to check if the player already exist
-      // we can allow player to make more than one bet
-      //require(!checkPlayerExists(msg.sender));
-      //The second one is used to see if the value sended by the player is
-      //Higher than the minimum value
+    function placeBet(uint8 team) public payable {
+
+      require(team == 1 || team == 2);
+
       require(msg.value >= minimumBet);
-      //set the player informations : amount of the bet and selected team
+
       playerInfo[msg.sender].amount = msg.value;
-      playerInfo[msg.sender].team = _team;
-      //then we add the address of the player to the players array
+      playerInfo[msg.sender].team = team;
+
       players.push(msg.sender);
-      //at the end, we increment the stakes of the team selected with the player bet
-      if ( _team == 1){
+
+      if (team == 1){
           sumBetsFirstTeam += msg.value;
       }
       else{
@@ -51,17 +49,17 @@ contract BetContract {
 
     function distributePrizes(uint16 winnerTeam) public {
 
+      require(winnerTeam == 1 || winnerTeam == 2 || winnerTeam == 3);
+
       address payable[1000] memory winners;
-      uint256 count = 0; // This is the count for the array of winners
-      uint256 LoserBet = 0; //This will take the value of all losers bet
-      uint256 WinnerBet = 0; //This will take the value of all winners bet
+      uint256 count = 0;
+      uint256 LoserBet = 0;
+      uint256 WinnerBet = 0;
       address add;
       uint256 bet;
       address payable playerAddress;
-      //define which bet sum is the Loser one and which one is the winner
 
       if(winnerTeam == 3){ //teams have drew
-          //loop through the player array to check who selected the winner team
           for(uint256 i = 0; i < players.length; i++){
              playerAddress = players[i];
              bet = playerInfo[playerAddress].amount;
@@ -82,9 +80,19 @@ contract BetContract {
 
       if(WinnerBet == 0 && LoserBet == 0)
         return;
-      //loop through the array of winners, to give ethers to the winners
+
+      for(uint256 i = 0; i < players.length; i++){
+        playerAddress = players[i];
+
+        if(playerInfo[playerAddress].team == winnerTeam){
+          winners[count] = playerAddress;
+          count++;
+        }
+      }
+
+
       for(uint256 j = 0; j < count; j++){
-          // Check that the address in this fixed array is not empty
+
          if(winners[j] != address(0))
             add = winners[j];
             bet = playerInfo[add].amount;
@@ -92,9 +100,9 @@ contract BetContract {
             winners[j].transfer((bet*(10000+(LoserBet*10000/WinnerBet)))/10000);
       }
 
-      delete playerInfo[playerAddress]; // Delete all the players
-      players.length = 0; // Delete all the players array
-      LoserBet = 0; //reinitialize the bets
+      delete playerInfo[playerAddress];
+      players.length = 0;
+      LoserBet = 0;
       WinnerBet = 0;
       sumBetsFirstTeam = 0;
       sumBetsSecondTeam = 0;
